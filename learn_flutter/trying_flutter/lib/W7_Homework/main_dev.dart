b3ee1ea84c3ff1ed32c12985633e2109d7f8b621
Vorak Tong <viraktong197@gmail.com>
2026-03-05 22:22:53 +0700

    Inject user history repo

--- learn_flutter/trying_flutter/lib/W7_Homework/main_dev.dart
+++ learn_flutter/trying_flutter/lib/W7_Homework/main_dev.dart
@@ -1,10 +1,12 @@
 import 'package:provider/provider.dart';
 import 'package:nested/nested.dart';
- 
+
 import 'main_common.dart';
 import 'data/repositories/settings/app_settings_repository_mock.dart';
 import 'data/repositories/songs/song_repository.dart';
 import 'data/repositories/songs/song_repository_mock.dart';
+import 'data/repositories/user_history/user_history_repository.dart';
+import 'data/repositories/user_history/user_history_repository_mock.dart';
 import 'ui/states/player_state.dart';
 import 'ui/states/settings_state.dart';
 
@@ -21,8 +23,11 @@ List<SingleChildWidget> get devProviders {
 
     // 3 - Inject the  app setting state
     ChangeNotifierProvider<AppSettingsState>(
-      create: (_) =>AppSettingsState(repository: appSettingsRepository)
+      create: (_) => AppSettingsState(repository: appSettingsRepository),
     ),
+
+    // 4 - Inject the user history repository
+    Provider<UserHistoryRepository>(create: (_) => UserHistoryRepositoryMock()),
   ];
 }
 
